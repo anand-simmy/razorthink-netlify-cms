828d7fe499d335186d4a079bf2cf6cc7b646e397
anand simmy <anand.simmy@razorthink.com>
2021-01-20 10:52:08 +0530

    layout changes

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ import "./components.css"
 const Header = () => {
     return (
         <div className="header">
-            <h1>React Markdown Blog</h1>
+            <h1>Razorthink Help Documentation</h1>
         </div>
     )
 }
